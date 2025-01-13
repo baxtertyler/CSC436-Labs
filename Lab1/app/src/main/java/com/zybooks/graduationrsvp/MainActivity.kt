4226f29a9ebf4a23d61ff93e4fade2c027638fb2
Tyler Baxter <tybaxter@calpoly.edu>
2025-01-13 13:00:55 -0800

    date

--- Lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ Lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -37,20 +42,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = stringResource(R.string.graduation_cap),
-        alpha = 0.3f
-    )
-    Text(
-        text = stringResource(R.string.graduation_announcement),
-        fontSize = 50.sp,
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 50.sp,
-        modifier = modifier
-    )
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier.fillMaxHeight()
+    ) {
+        Text(
+            text = stringResource(R.string.graduation_announcement),
+            fontSize = 50.sp,
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 50.sp,
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(R.drawable.grad_cap),
+            contentDescription = stringResource(R.string.graduation_cap),
+            alpha = 0.3f
+        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text (
+                text = "May 14 - Ganus Hall - 2:00 pm",
+                fontSize = 20.sp
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
