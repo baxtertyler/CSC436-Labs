9c94919e1f08f4b09a59a3ccf60157428baf34c2
Tyler Baxter <tybaxter@calpoly.edu>
2025-01-16 15:56:58 -0800

    init lab2

--- Lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ Lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -62,12 +67,24 @@ fun GradScreen(modifier: Modifier = Modifier) {
         )
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text (
-                text = "May 14 - Ganus Hall - 2:00 pm",
+                text = stringResource(R.string.date),
                 fontSize = 20.sp
             )
         }
+        RSVP()
 
     }
+}
+
+@Composable
+fun RSVP() {
+    var isChecked by remember { mutableStateOf(true) }
+    val message = if (isChecked) "I'll be there" else "Sorry I can't be there"
+    Switch(
+        checked = isChecked,
+        onCheckedChange = { x:Boolean -> isChecked = x }
+    )
+    Text(text = message)
 }
 
 @Preview(showBackground = true)
